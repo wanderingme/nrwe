3d3b29f2f2e704c7e317f4415033a6e603fa3d41
wanderm <me@me.meme>
2023-01-29 15:32:39 -0800

    Stupid youtube message taken down

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-        </p>
-        <div>Hey YouTube, yes we're the copyright owners of The World Over. I don't know what else you require from us. We're not setting up a corporate email for you so hopefully posting this message on our website will be enough.</div>
       </header>
      
     </div>
